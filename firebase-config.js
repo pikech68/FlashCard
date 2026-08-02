6cf20d31c499fe0b2bd6d6177dcfd8f02b58c4fe
pikech68 <chiharu0268@gmail.com>
2026-08-02 18:50:28 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDPI01obkC-bLCHPDYyqHbRZLWOTv2b0VM",
+  authDomain: "career-flashcard.firebaseapp.com",
+  projectId: "career-flashcard",
+  storageBucket: "career-flashcard.firebasestorage.app",
+  messagingSenderId: "751327295998",
+  appId: "1:751327295998:web:8ceb6809df44c962dba8af",
+  measurementId: "G-EEDYNS53HB"
+};
